22ba2a5214cfcaff5ee973a2b2959fbfb1f1bd9a
antonio pacehco <antonio_pacheco@outlook.com>
2020-11-11 21:28:26 -0800

    feat(app): change default main text
    fix #1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This is edited
         </p>
         <a
           className="App-link"
